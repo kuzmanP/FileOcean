da58f1ce11d10d4bd9b28948217d5290247a969e
Kuzman <georgeghartey2@gmail.com>
2023-01-19 17:24:38 +0000

    deleted render to response(users)

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect,render_to_response
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.views import generic 
 from django.db.models import Q
