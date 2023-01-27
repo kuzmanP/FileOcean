8a7a2e2e3e34d182cf1cb23159fc084c5e550eb5
Kuzcodes <georgeghartey2@gmail.com>
2023-01-27 16:47:45 +0000

    feat: Added a redirect route after success API login(apis)

--- apis/views.py
+++ apis/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.schemas import SchemaGenerator
@@ -115,6 +115,7 @@ class LoginView(APIView):
         }
 
         return response
+        return redirect("/")
 
 #Using Cookies to retrieve The users        
 class UserView(APIView):
