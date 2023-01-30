bef2cec15d9437d494a38406d8ef3f28f9eeee9f
KuzmanP <georgeghartey2@gmail.com>
2023-01-30 11:34:47 +0000

    chore: updated the access token from 1 minute to 60 mins(FileOcean)

--- FileOcean/settings.py
+++ FileOcean/settings.py
@@ -135,7 +135,7 @@ WSGI_APPLICATION = 'FileOcean.wsgi.application'
 
 #JWT Settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
