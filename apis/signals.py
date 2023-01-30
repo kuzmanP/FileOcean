2e14f7fa598783b43363ccb04f17a7884fa7672e
KuzmanP <georgeghartey2@gmail.com>
2023-01-30 11:58:23 +0000

    chore: Updated the email content(FileOcean)

--- apis/signals.py
+++ apis/signals.py
@@ -11,11 +11,11 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
 
     send_mail(
         # title:
-        "Password Reset for {title}".format(title="Some website title"),
+        "Password Reset for {title}".format(title="FileOcean Cloud Storage"),
         # message:
         email_plaintext_message,
         # from:
-        "noreply@somehost.local",
+        "noreply@FileOcean.local",
         # to:
         [reset_password_token.user.email]
     )
